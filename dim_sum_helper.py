81699e769fe8579304daf7886702ab7f93e90096
Michelle Y Ho <michelle.y.ho@gmail.com>
2021-01-26 20:15:21 -0800

    helper function.
    Simple way of storing information of various dim sum dishes.  Use dictionary for now.  Migrate to database later.

---
+++ dim_sum_helper.py
@@ -0,0 +1,52 @@
+dim_sum_dish={
+    1: "Shrimp Dumpling", 
+    2: "Pork Dumpling", 
+    3: "Beef Ball Dumpling",
+    4: "Shrimp Rice Roll",
+    5: "Beef Rice Roll",
+    6: "Steamed BBQ Pork Buns",
+    7: "Baked BBQ Pork Buns",
+    8: "Egg Tarts",
+    9: "BBQ Pork Puff Pastry",
+}
+
+chinese_name={
+    1: "Ha Gow", 
+    2: "Siu Mai", 
+    3: "Au Yuk Siu Mai",
+    4: "Ha Cheung",
+    5: "Au Cheung",
+    6: "Tsing Cha Siu Bow",
+    7: "Kok Cha Siu Bow",
+    8: "Daan Tat",
+    9: "Cha Siu So",
+}
+
+description={
+
+
+}
+
+dim_sum_img={
+    1:"https://i.pinimg.com/originals/26/2e/23/262e236cb9078caec7b39525c5680011.jpg",
+    2:"https://i.ytimg.com/vi/k3JLpHzPVNo/maxresdefault.jpg",
+    3:"https://upload.wikimedia.org/wikipedia/commons/e/e0/HK_Pacific_Plaza_SYP_%E5%BE%B7%E9%9F%BE%E8%8B%91_Tak_Hing_Yuen_Seafood_Restaurant_beef_meat_balls_Mar-2013_Bamboo_steamer.JPG",
+    4:"https://i.pinimg.com/originals/ff/f9/64/fff964ad9b18ef4a35ec1eab357149ca.jpg",
+    5:"https://i.pinimg.com/originals/51/65/9b/51659bbd263e418accc2ca097db7b8cd.jpg",
+    6:"https://dianacookswithlove.files.wordpress.com/2012/11/bbqporkbuns1.jpg",
+    7:"https://i.pinimg.com/originals/5d/f8/68/5df8685d6bb5cbeeaa0bbc811291ef85.jpg",
+    8:"https://ailengfood.weebly.com/uploads/3/0/2/3/30235133/2904090_orig.jpg?321",
+    9:"https://yumchajunkies.files.wordpress.com/2009/08/charsiuso.jpg?w=376&h=282",
+}
+
+cook_method={
+    1: "steamed", 
+    2: "steamed", 
+    3: "steamed",
+    4: "steamed",
+    5: "steamed",
+    6: "steamed",
+    7: "baked",
+    8: "baked",
+    9: "baked",
+}
